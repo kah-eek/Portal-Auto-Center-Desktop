080cedbd91a83413c40d3a3f14326f3186095188
CAIQUE MEIRA OLIVEIRA <16254840@CFP127ALUNO.LOCAL>
2018-05-14 11:22:25 -0300

    Backup do DB

--- database/query/generalQuery.sql
+++ database/query/generalQuery.sql
@@ -1,4 +1,13 @@
 use db_auto_center;
 
 SELECT * FROM tbl_funcionario_pac;
+SELECT * FROM tbl_usuario;
 
+SELECT * FROM tbl_pagamento_funcionario_pac;
+DELETE FROM tbl_pagamento_funcionario_pac WHERE id_pagamento_funcionario_pac != -1;
+
+insert into tbl_pagamento_funcionario_pac (id_funcionario_pac)
+ select id_funcionario_pac from tbl_funcionario_pac;
+ 
+ INSERT INTO tbl_pagamento_funcionario_pac (id_funcionario_pac) 
+SELECT id_funcionario_pac FROM tbl_funcionario_pac;
